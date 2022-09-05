fe55dfd65f3a81824a67f9ca2e2587e1fc7dadc6
DenisSofronov <sofronovd1@yandex.ru>
2022-09-05 17:32:01 +0500

    commit work complete

--- 2.functions/main.js
+++ 2.functions/main.js
@@ -1,7 +1,7 @@
-console.log(getArrayParams([100, -100, 50]));
-console.log(getArrayParams([10, 20, 30]));
-console.log(getArrayParams([1, -4, 7]));
+console.log(getArrayParams([120, -150, 15, 132]));
+console.log(getArrayParams([-100, 48, 76]));
+console.log(getArrayParams([1, 4, 7]));
 
-console.log(makeWork([[10,10,20],[20,19,10]],worker));
-console.log(makeWork([[12,23,31],[41,5,61]],worker));
-console.log(makeWork([[14,21,36],[15,26,39]],worker));
+console.log(makeWork([[10,15,26],[20,19,10]],worker));
+console.log(makeWork([[11,30,43],[36,43,111]],worker));
+console.log(makeWork([[19,-25,36],[-19,33,42]],worker));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
-// Задание 1
+'use strict';
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min,max,sum,avg;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  min = arr[0];
+  max = arr[0];
+  sum = arr[0];
+  for (let i = 1; i < arr.length; i += 1) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = +(sum / arr.length).toFixed(2);
+  return {
+    min: min,
+    max: max,
+    avg: avg
+  };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  for (let value of arr) {
+    sum += value;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+  let max = 0;
+   let sum;
+   for (let value of arrOfArr) {
+     sum = func(value);
+     if (max < sum) {
+       max = sum;
+     }
+   }
+   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, difference;
+min = arr[0];
+max = arr[0];
+for (let i = 1; i < arr.length; i += 1) {
+  if (min > arr[i]) {
+    min = arr[i];
+  }
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+}
+difference = max - min;
+return difference;
 }
