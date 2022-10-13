6eb41254c4cf3cd32655a2505260a4107ad72eb5
DenisSofronov <sofronovd1@yandex.ru>
2022-10-13 20:12:19 +0500

    Commit decorators revision

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,43 +1,35 @@
+'use strict';
+
 function cachingDecoratorNew(func) {
   let cache = [];
 
-   function wrapper(...args) {
-     const hash = args.join(',');
-     let idx = cache.findIndex((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
-     if (idx !== -1) { // если элемент не найден
-       console.log("Из кэша: " + cache[idx].value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-       return "Из кэша: " + cache[idx].value;
-     } else {
-       let result = func(...args); // в кэше результата нет - придётся считать
-       cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
-       if (cache.length > 5) {
-         cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
-       }
-       console.log("Вычисляем: " + result);
-       return "Вычисляем: " + result;
-     }
-   }
-   return wrapper;
- }
+  function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+    let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1 ) { // если элемент не найден
+      //cache[idx] = func(...args);
 
- function debounceDecoratorNew(f, ms) {
-   let isCooldown = false;
-   return function () {
-     if (isCooldown) return;
-     f.apply(this, arguments);
-     isCooldown = true;
-     setTimeout(() => isCooldown = false, ms);
-   };
- }
+      console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + cache[idx].result;
+    }
 
- function debounceDecorator2(func, ms) {
-  let isCooldown = false;
-  let count;
-  return function () {
-    if (isCooldown) return;
-    func.apply(this, arguments);
-    isCooldown = true;
-    setTimeout(() => isCooldown = false, ms);
-    count++;
-  };
+    let result = func(...args); // в кэше результата нет - придётся считать
+    //cache.push(hash) ; // добавляем элемент с правильной структурой
+    cache.push({hash, result});
+    if (cache.length > 5) {
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
+  }
+
+function debounceDecoratorNew(func) {
+  // Ваш код
+}
+
+function debounceDecorator2(func) {
+  // Ваш код
 }
