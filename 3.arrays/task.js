b99eadf724d8f35b9f12d73c1b3f7d1ecba72907
DenisSofronov <sofronovd1@yandex.ru>
2022-10-13 19:54:21 +0500

    Commit arrays revision

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
-
   result =
   arr1.length === arr2.length &&
-  arr1.every((number) => arr1[number] === arr2[number]);
+  arr1.every((v,i)=>v === arr2[i])
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
   resultArr = arr
     .filter((number) => number > 0)
     .filter((number) => number % 3 === 0)
     .map((number) => number * 10);
-    
+
   return resultArr; // array
 }
